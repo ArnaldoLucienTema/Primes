0298a3f5b6208a3e1a794136005dc62edd852caf
ArnaldoLucienTema <arnaldo.trindade@closer>
2018-10-16 01:37:29 +0100

    beutifying the table through hbs

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,15 @@ let express = require('express');
 let router = express.Router();
 let primes_table = require('./../primes-table/processor');
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
 
     let n = parseInt(req.query.n);
 
-    let arr = primes_table._generatePrimeArray(n);
-    console.log(arr);
-    res.render('index', {title: 'Express'});
+    let primes = primes_table._generatePrimeArray(n);
+
+    console.log(primes);
+
+    res.render('primes', {primes: primes});
 });
 
 module.exports = router;
